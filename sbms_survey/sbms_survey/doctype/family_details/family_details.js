be423b662af172f1fa8fb3544d3d94ef4378fe7a
shreshtadev <chinmai.bharadwaj@ymail.com>
2024-03-10 11:43:57 +0530

    Family Details with Members

---
+++ sbms_survey/sbms_survey/doctype/family_details/family_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, shreshtadev and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Family Details", {
+// 	refresh(frm) {
+
+// 	},
+// });
